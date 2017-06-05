a05886b42b49cf9cbf578318c33f76cccdc2b775
huangxinguang <hxguang911219@163.com>
2017-06-05 14:16:29 +0800

    注册中心

---
+++ springcloud-eurekaServer/src/main/java/com/starlight/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.starlight.controller;
+
+import com.netflix.appinfo.InstanceInfo;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by huangxinguang on 2017/6/5 上午11:45.
+ */
+@RestController
+public class HelloController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
+    public String add(@RequestParam String name) {
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("STORES");
+        for(ServiceInstance serviceInstance:instanceList) {
+            logger.info("/hello, host:" + serviceInstance.getHost() + ", service_id:" + serviceInstance.getServiceId() + ", hello:" + name);
+        }
+        return "hello:"+name;
+    }
+}
